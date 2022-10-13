4f6b44dcb39bb7062dfbabfd65df130563672251
vickey <shrivastav.vickey432@gmail.com>
2022-10-13 15:45:16 +0530

    updated

--- app.js
+++ app.js
@@ -1,21 +1,35 @@
 const http = require('http');
 const fs = require ('fs')
-
+//file system core module
 const server =http.createServer((req , res)=>{
 const url = req.url;
 const method = req.method;
 
+//routing request
+
 if(url==='/')
   { 
 res.write('<html>');
-res.write('<body><form action = "/message " method = "post"><input type = "text" name = "message"><button type = "submit">send</button></form></body>');
+res.write('<body><form action = "/message " method = "post"><input type="text" name = "message"><button type = "submit">send</button></form></body>');
 res.write('</html>');
 return res.end();
 
   }
   if(url ==='/message' && method === 'post')
   {
-fs.writeFileSync('message.text','dummmy');
+    const body =[];
+    req.on('data', (chunk)=>{
+      console.log(chunk);
+      body.push(chunk);
+    });
+
+    req.on('end',()=>{
+      const parsedBody = Buffer.concat(body).toString();
+      const message = parsedBody.split('=')[1];
+      fs.writeFileSync('message.txt','dummmy');
+      console.log(parsedBody);
+    });
+
 res.statusCode=302;
 res.setHeader('location', '/');
 res.send.end(); 
@@ -23,10 +37,9 @@ res.send.end();
   }
     res.setHeader('content-type', 'text/html');
     res.write('<html>');
-    res.write('<body><h1>hello node</h1></body>');
+    res.write('<body><h1>Processing....</h1></body>');
     res.write('</html>');
     res.end();
 });
 
 server.listen(3000);
-console.log("server is running at port")
